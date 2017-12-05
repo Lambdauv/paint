75c111ecff2ec110be093b9165f11cf8a642c12b
roseldon <Roseldon@Jies-MacBook-Air.local>
2017-12-04 22:33:39 -0800

    _

--- paint.py
+++ paint.py
@@ -1,1 +1,9 @@
-import 
+######################################################################
+#                                                                    #
+#  Copyright 2017- Roger Luo.                                        #
+#  This file is part of paint, distributed under the terms of the    #
+#  MIT License                                                       #
+#                                                                    #
+######################################################################
+import gdspy
+import numpy
