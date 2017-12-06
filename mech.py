ea1dc27897b24afb47679f77126ed4e49390a5fb
roseldon <Roseldon@dhcp-238-69.caltech.edu>
2017-12-06 14:46:03 -0800

    modified imports in __init__.py and created a file for dealing with mechanical strctures

--- mech.py
+++ mech.py
@@ -3,7 +3,15 @@
 #  Copyright 2017- Lambdauv.                                        #
 #  This file is part of paint, distributed under the terms of the    #
 #  MIT License                                                       #
+#  
+#  This module contains classes for constructing Mechanical elements #
 ######################################################################
 import gdspy
 import numpy
 import operator
+
+class holes(object):
+    def __init__(self,name,arrayaxes=((1,0),(0,1)),arrayperiods=(1,1)):
+        self.Name=name
+        self.Axes=arrayaxes
+        self.Periods=arrayperiods
