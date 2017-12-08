a9c69677b5ac881331eadd6face0daf2351aae9c
Lambdauv <>
2017-12-08 14:29:02 -0800

    the crosses can now have fillet corrections embedded

--- mech.py
+++ mech.py
@@ -42,13 +42,35 @@ class holes(object):
         else:
             raise ValueError("Error in the holecell input! Not right dimention")
 class cross(object):
-    def __init__(self,ct,ch,layer=0,datatype=0):
+    def __init__(self,ct,ch,fillet=0,layer=0,datatype=0):
         self.spec={'layer': layer, 'datatype': datatype}
         Harm=gdspy.Path(ct,(-ch/2,0))
         Harm.segment(ch,0,**(self.spec))
         Varm=gdspy.Path(ct,(0,-ch/2))
         Varm.segment(ch,numpy.pi/2,**(self.spec))
         self.paths=[Harm,Varm]
+        if (fillet != 0):
+            filletwid=fillet*numpy.sqrt(2)
+            filletlen=filletwid/2+fillet
+            for psign in [-1,1]:
+                for qsign in [-1,1]:
+                    for len1 in [ct/2,ch/2]:
+                        for len2 in [ct/2,ch/2]:
+                            pt=(psign*len1,qsign*len2)
+                            if (len1 == ch/2)&(len2 == ch/2):
+                                continue
+                            elif (len1 == ct/2)&(len2 == ct/2):
+                                direction=(-psign/numpy.sqrt(2),-qsign/numpy.sqrt(2))
+                                p=gdspy.Path(filletwid,(pt[0]-direction[0]*filletwid/2,pt[1]-direction[1]*filletwid/2))
+                                #p=gdspy.Path(filletwid,pt)
+                                p.segment(filletlen,numpy.arctan2(direction[1],direction[0]),final_width=0)
+                                self.paths.append(p)
+                            else:
+                                direction=(psign/numpy.sqrt(2),qsign/numpy.sqrt(2))
+                                p=gdspy.Path(filletwid,(pt[0]-direction[0]*filletwid/2,pt[1]-direction[1]*filletwid/2))
+                                #p=gdspy.Path(filletwid,pt)
+                                p.segment(filletlen,numpy.arctan2(direction[1],direction[0]),final_width=0)
+                                self.paths.append(p)
     def add2cell(self,cell):
         self.cell=cell
         for p in self.paths:
